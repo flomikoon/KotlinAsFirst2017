3e4a6ad91e1db36f7440a59aba425d9d8d5f3ece
PavelKrynskiy <pasha.pele18@gmail.com>
2017-09-18 14:50:23 +0300

    Второй и первый урок

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -52,7 +52,7 @@ fun main(args: Array<String>) {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int) =
+fun seconds(hours: Int, minutes: Int, seconds: Int):Int =
         hours*3600+minutes*60+seconds
 
 /**
@@ -62,7 +62,7 @@ fun seconds(hours: Int, minutes: Int, seconds: Int) =
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int) =
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int):Double =
         sagenes*2.1336+arshins*0.7112+vershoks*0.04445
 
 /**
