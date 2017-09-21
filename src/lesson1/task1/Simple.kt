c53b342e508664a46358a391c38eee9d6a5e11a8
PavelKrynskiy <pasha.pele18@gmail.com>
2017-09-21 22:41:30 +0300

    Исправленный 1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -109,7 +109,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
 fun accountInThreeYears(initial: Int, percent: Int): Double =
-        initial* pow((1+percent/100.0), 3.0)
+        initial* pow(1+percent/100.0,3.0)
 
 /**
  * Простая
@@ -118,4 +118,4 @@ fun accountInThreeYears(initial: Int, percent: Int): Double =
  *Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
 fun numberRevert(number: Int): Int =
-        (number%10)*100+(number/10-number/100*10)*10+number/100
+        number%10*100+number%100/10*10+number/100
